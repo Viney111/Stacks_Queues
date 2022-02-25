810e68b3cb9d8a09af9587fdc1b1674865ac1631
Viney <vineykhaneja999@gmail.com>
2022-02-25 13:26:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stacks_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stacks_Methods<int> stacksQueuesMethods = new Stacks_Methods<int>();
+            stacksQueuesMethods.Push(70);
+            stacksQueuesMethods.Push(30);
+            stacksQueuesMethods.Push(56);
+            stacksQueuesMethods.DisplayInStacks();
+            
+
+
+        }
+    }
+}
---
+++ Stacks_Methods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Queues
+{
+    internal class Stacks_Methods<T>
+    {
+        public Node<T> top;
+        //For Adding in Stack.
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (top == null)
+            {
+                newNode.next = null;
+                Console.WriteLine($"{newNode.data} has been pushed in empty stack.");
+            }
+            else
+            {
+                newNode.next = top;
+                Console.WriteLine($"{newNode.data} has been pushed");
+            }
+            top = newNode;
+        }
+        //For display element in stacks
+        public void DisplayInStacks()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node<T> temp = top;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
